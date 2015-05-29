2189a99c53501e5c1fe958301e0d16e7ec795857
Christoph Neijenhuis <christoph.neijenhuis@commercetools.de>
2015-05-29 18:36:41 +0200

    Supports multiple files and . instead of a filename

--- README.md
+++ README.md
@@ -4,7 +4,15 @@ Takes a "$ref": "anotherSchema.schema.json", dereferences it, and inserts the co
 
 ## Usage
 
+You need sbt to run this project.
+
+Print inlined json schema to console:
 sbt "run https://raw.githubusercontent.com/sphereio/sphere-api-reference/master/schemas/customers.schema.json"
+Save inlined json into a file:
+sbt "run https://raw.githubusercontent.com/sphereio/sphere-api-reference/master/schemas/customers.schema.json customers-inlined.schema.json"
+You may use relative paths and '.' to use the original filename:
+sbt "run https://raw.githubusercontent.com/sphereio/sphere-api-reference/master/schemas/customers.schema.json ../another/dir/."
+You can also use a list of schema/file pairs
 
 Note: No cyclic references yet.
 
--- src/main/scala/RefInliner.scala
+++ src/main/scala/RefInliner.scala
@@ -15,28 +15,52 @@ import org.json4s._
 import org.json4s.native.JsonMethods._
 
 object RefInliner {
-  val baseUrl = "http://localhost:8080/sphere/schemas/"
-  //val schemaUrl = baseUrl + "categories.schema.json"
-  val schemaUrl = baseUrl + "customers.schema.json"
-
-  def splitBaseUrlAndSchema(str: String): (String, String) = {
-    val i = str.lastIndexOf("/")
-    if (i < 1) throw new IllegalArgumentException("Not a valid url")
-    str.splitAt(i + 1)
+  def printUsage() = {
+    println("Usage:")
+    println("Print inlined json schema to console:")
+    println("https://raw.githubusercontent.com/sphereio/sphere-api-reference/master/schemas/customers.schema.json")
+    println("Save inlined json into a file:")
+    println("https://raw.githubusercontent.com/sphereio/sphere-api-reference/master/schemas/customers.schema.json customers-inlined.schema.json")
+    println("You may use relative paths and '.' to use the original filename:")
+    println("https://raw.githubusercontent.com/sphereio/sphere-api-reference/master/schemas/customers.schema.json ../another/dir/.")
+    println("You can also use a list of schema/file pairs")
   }
 
   def main(args: Array[String]): Unit = {
-    val (base, schema) = if (args.size > 0) splitBaseUrlAndSchema(args(0)) else (baseUrl, schemaUrl)
-    val inliner = new RefInliner(base, schema)
-    val result = Await.result(inliner.inlineRefs(), 5 seconds)
-
-    if (args.size > 1) {
-      Files.write(Paths.get(args(1)), pretty(render(result)).getBytes(StandardCharsets.UTF_8))
-    } else println(pretty(render(result)))
+    if (args.size == 0 || (args.size > 2 && args.size % 2 == 1)) {
+      printUsage()
+    }
+    else if (args.size == 1) {
+      val inliner = new RefInliner(args(0))
+      val result = Await.result(inliner.inlineRefs(), 5 seconds)
+      println(pretty(render(result)))
+    }
+    else {
+      val futures = args.toList.grouped(2).map { _ match {
+        case u :: f :: List() => 
+          val inliner = new RefInliner(u)
+          inliner.inlineRefs() map { json => 
+            pretty(render(json)).getBytes(StandardCharsets.UTF_8) 
+          } map { str =>
+            val fileName = if (f.endsWith(".")) f.slice(0, f.size - 1) + inliner.schema else f
+            Files.write(Paths.get(fileName), str)
+          }
+        case _ => Future.failed(new IllegalArgumentException)
+      } }
+      Await.result(Future.sequence(futures), (futures.size + 5) seconds)
+    }
   }
 }
 
-class RefInliner(baseUrl: String, schema: String) {
+class RefInliner(urlOfSchema: String) {
+  def splitBaseUrlAndSchema(str: String): (String, String) = {
+    val i = str.lastIndexOf("/")
+    if (i < 1) throw new IllegalArgumentException(s"Not a valid url: $str")
+    str.splitAt(i + 1)
+  }
+
+  val (baseUrl, schema) = splitBaseUrlAndSchema(urlOfSchema)
+
   lazy val h: Http = new Http(new AsyncHttpClient)
 
   def inlineRefs() = {
